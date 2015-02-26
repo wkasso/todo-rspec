42ea80993bdecd4fac916a478dc581bd5ecc2c4c
Mohammed Kasso <mkasso@spartaglobal.com>
2015-02-26 16:44:30 +0000

    Added yaml data file

--- spec/todo_spec.rb
+++ spec/todo_spec.rb
@@ -1,8 +1,11 @@
 require 'spec_helper'
 require 'httparty'
+require 'yaml'
 
 describe "ToDo" do
   let(:rget) {rget = HTTParty.get "http://lacedeamon.spartaglobal.com/todos"}
+  let(:data) {data = YAML.load(File.open('data.yaml'))}
+
   it "Should post" do
     expect(rget.code).to eq(200)
   end
