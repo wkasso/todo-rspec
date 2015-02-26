db977f5695e0e451f145a99b5e0d73a47979cebc
Mohammed Kasso <mkasso@spartaglobal.com>
2015-02-26 17:56:55 +0000

    Added create an item test

--- spec/todo_spec.rb
+++ spec/todo_spec.rb
@@ -5,8 +5,16 @@ require 'yaml'
 describe "ToDo" do
   let(:rget) {rget = HTTParty.get "http://lacedeamon.spartaglobal.com/todos"}
   let(:data) {data = YAML.load(File.open('data.yaml'))}
-
-  it "Should post" do
+  let(:entry) {entry = data["list"]}
+  it "Should get collection" do
     expect(rget.code).to eq(200)
   end
+
+  it "Create an item" do 
+    rpost = HTTParty.post "http://lacedeamon.spartaglobal.com/todos", query: entry[0]
+    id = rpost["id"]
+    expect(rpost.code).to eq(201)
+    HTTParty.delete "http://lacedeamon.spartaglobal.com/todos/#{id}"
+  end
+
 end
